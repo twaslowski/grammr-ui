40a69f5cb78f928af2d3aa212a304d18f1b5168b
Tobias Waslowski <tobias.waslowski@otto-payments.de>
2025-02-28 09:13:18 +0100

    fix: remove pnpm version in github actions
    conflicts with version from package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,16 +26,7 @@ module.exports = {
 
     //#region  //*=========== Unused Import ===========
     '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'warn',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
+    'unused-imports/no-unused-imports': 'off',
     //#endregion  //*======== Unused Import ===========
 
     //#region  //*=========== Import Sort ===========
